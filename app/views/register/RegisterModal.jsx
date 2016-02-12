58cdbd836a682159de9b374dae7a37cc25c090a5
Dylan O'Carroll <dylancocarroll@gmail.com>
2016-02-12 15:11:07 -0800

    style register modal

--- app/views/register/RegisterModal.jsx
+++ app/views/register/RegisterModal.jsx
@@ -17,6 +17,15 @@ var RegisterView = React.createClass({
                 <div className="modal-dialog narrow-modal">
                     <div className="modal-content">
                         <div className="modal-body">
+                            <div className="ps15">
+                                <button onClick={this.FBLogin} type="button" className="btn btn-default fb-blue mb4 ps20">
+                                    <i className="fa fa-facebook-f mr10"></i>
+                                    Log in with Facebook
+                                </button>
+                            </div>
+                            <div className="login-or">or</div>
+                            <div className="pt20 btddd w100p"></div>
+
                             <Basic />
                             <div className="ms20 tos-container">
                                 <div className="terms-of-service mt20 mb20">
